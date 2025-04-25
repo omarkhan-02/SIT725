bcec70c66c529724e2aa1d654a1e0c8bde324ea5
Omarkhan <s225027671@deakin.edu.au>
2025-04-26 09:31:34 +1000

    new changes

---
+++ Task_7.2P/client.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+
+const client = net.createConnection({ port: 5000 }, () => {
+    console.log('Connected to server');
+
+    client.write('Hello from client!');
+});
+
+client.on('data', (data) => {
+    console.log('Received from server:', data.toString());
+    client.end(); 
+});
+
+
+client.on('end', () => {
+    console.log('Disconnected from server');
+});
---
+++ Task_7.2P/server.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const server = net.createServer((socket) => {
+    console.log('Client connected');
+
+
+    socket.on('data', (data) => {
+        console.log('Received from client:', data.toString());
+
+
+        socket.write('Hello from server!');
+    });
+
+
+    socket.on('end', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
+server.listen(5000, () => {
+    console.log('Server listening on port 5000');
+});
